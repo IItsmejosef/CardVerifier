f1ee7912bd51e3c8417559798610c62690f1cb41
IItsmejosef <55672703+IItsmejosef@users.noreply.github.com>
2020-03-12 01:27:23 +0000

    Update CardVerifier.py

--- CardVerifier.py
+++ CardVerifier.py
@@ -1,1 +1,32 @@
+def digitSum(myString):
 
+    length = len(myString)
+    oddSum = 0
+    evenSum = 0
+
+    if (length == 0):
+        return 0
+
+    else:
+        if length % 2 == 0:
+            last = int(myString[-1])
+            evenSum += last
+            return evenSum + digitSum(myString[:-1])
+        else:
+            last = int(myString[-1])
+            last = 2 * last
+            part_sum = last // 10 + last % 10
+            oddSum += part_sum
+
+            return oddSum + digitSum(myString[:-1])
+
+def CardVerifier():
+    myString = input('Enter 16 digit card number: ')
+    total = digitSum(myString)
+    if (total % 10 == 0):
+        print('Valid Card')
+    else:
+        print('Invalid Card')
+
+
+CardVerifier()
